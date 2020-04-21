0358bd6f6503847cd821d3797ebdb9e3e33a6610
johan guxman <johan.dev19@gmail.com>
2020-04-21 09:35:24 -0400

    add a process.env.PORT to express serve

--- startServer.js
+++ startServer.js
@@ -8,6 +8,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
