b5ae5866ed027c295acc96821a929d209ef5420e
johan guzman <johan.dev19@gmail.com>
2020-09-05 22:59:51 -0400

    refactor the label component

--- src/components/Label/labelStyle.js
+++ src/components/Label/labelStyle.js
@@ -1,20 +1,20 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const LabelStyle = styled.div`
   margin-bottom: 10px;
+`;
 
-  .title {
-    display: block;
-    font-size: 14px;
-    color: #f08080;
-    font-weight: bold;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-  }
+export const Title = styled.span`
+  display: block;
+  font-size: 14px;
+  color: #f08080;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+`;
 
-  .content {
-    display: block;
-    font-size: 20px;
-    color: #6c757d;
-  }
+export const Content = styled.span`
+  display: block;
+  font-size: 20px;
+  color: #6c757d;
 `;
